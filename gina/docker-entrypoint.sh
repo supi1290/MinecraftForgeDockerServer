e4b8880aa49d4bd62baa8cdbd92b068c242355d5
Haider Sebastian <supi1290@gmail.com>
2020-04-10 09:31:41 +0200

    Update remove Unzipped foldername

--- gina/docker-entrypoint.sh
+++ gina/docker-entrypoint.sh
@@ -21,7 +21,7 @@ if [[ ! -f "$FILE" ]]; then
     mv ${MODPACK_FILENAME} server.zip
     unzip server.zip
     rm server.zip
-    rm ${MODPACK_UNZIPPED_FOLDERNAME}
+    rm -rf ${MODPACK_UNZIPPED_FOLDERNAME}
 
     cat <<- EOF >/opt/mcserver/server/eula.txt
         #By changing the setting below to TRUE you are indicating your agreement to our EULA (https://account.mojang.com/documents/minecraft_eula).
